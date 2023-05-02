212775d175acf1a5e91ee96480028dc5c1d492f6
Mahmoud Hosny Imran <mahmoudhosnyomran@gmail.com>
2023-05-02 09:01:17 +0300

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import CustomButton from './CustomButton.jsx';
+import AIPicker from './AIPicker.jsx';
+import ColorPicker from './ColorPicker.jsx';
+import FilePicker from './FilePicker.jsx';
+import Tab from './Tab.jsx';
+
+
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
